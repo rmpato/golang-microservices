89b38982b5f66076e7686e4913ef83c7eaf67126
pato <pato.rodriguez10@gmail.com>
2021-09-22 11:58:00 -0300

    adds a constructor func to initialize the event handler with a db handler injected

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"myevents/persistence"
 	"net/http"
 )
 
@@ -16,25 +17,24 @@ func ServeAPI() {
 	eventsrouter := r.PathPrefix("/events").Subrouter()
 
 	//Retrieves events by search criteria by GET
-	eventsrouter.Methods("GET").
-		Path("/{SearchCriteria}/{search}").
-		HandlerFunc(handler.findEventHandler)
+	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
 
 	//Retrieves all events by GET
-	eventsrouter.Methods("GET").
-		Path("").
-		HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
 
 	//Creates a new Event from a POST request
-	eventsrouter.Methods("POST").
-		Path("").
-		HandlerFunc(handler.newEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 
 	http.ListenAndServe(":8181",r)
 }
 
-type eventServiceHandler struct {
+//A costructor func to build the event service handler with a db handler injected
+func newEventHandler(databaseHandler persistence.DatabaseHandler) *eventServiceHandler {
+	return &eventServiceHandler{dbhandler: databaseHandler}
+}
 
+type eventServiceHandler struct {
+	dbhandler persistence.DatabaseHandler
 }
 
 func (eh *eventServiceHandler) findEventHandler(w http.ResponseWriter, r *http.Request)  {
