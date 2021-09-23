846da061492857da7bb2aa077099a1becb1726bd
pato <pato.rodriguez10@gmail.com>
2021-09-23 12:11:17 -0300

    adds new event handler to create events on post

--- main.go
+++ main.go
@@ -97,5 +97,17 @@ func (eh *eventServiceHandler) allEventHandler(w http.ResponseWriter, r *http.Re
 }
 
 func (eh *eventServiceHandler) newEventHandler(w http.ResponseWriter, r *http.Request) {
-
+	event := persistence.Event{}
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{error: error occured while decoding event data %s}", err)
+		return
+	}
+	id, err := eh.dbhandler.AddEvent(event)
+	if nil != err {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{error: error occured while persisting event %d %s}", id, err)
+		return
+	}
 }
