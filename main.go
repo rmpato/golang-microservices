adcb3156e18fbd9f691bd1b8166630f0bfedf317
pato <pato.rodriguez10@gmail.com>
2021-09-23 11:36:58 -0300

    implements find events by name or id handler

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"myevents/persistence"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -25,7 +29,7 @@ func ServeAPI() {
 	//Creates a new Event from a POST request
 	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 
-	http.ListenAndServe(":8181",r)
+	http.ListenAndServe(":8181", r)
 }
 
 //A costructor func to build the event service handler with a db handler injected
@@ -37,14 +41,48 @@ type eventServiceHandler struct {
 	dbhandler persistence.DatabaseHandler
 }
 
-func (eh *eventServiceHandler) findEventHandler(w http.ResponseWriter, r *http.Request)  {
+func (eh *eventServiceHandler) findEventHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	criteria, ok := vars["SearchCriteria"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, `{error: No search criteria found, you can either search by id via /id/4
+                   to search by name via /name/coldplayconcert}`)
+		return
+	}
+
+	searchkey, ok := vars["search"]
+	if !ok {
+		w.WriteHeader(400)
+		fmt.Fprint(w, `{error: No search keys found, you can either search by id via /id/4
+                   to search by name via /name/coldplayconcert}`)
+		return
+	}
+
+	var event persistence.Event
+	var err error
+	switch strings.ToLower(criteria) {
+	case "name":
+		event, err = eh.dbhandler.FindEventByName(searchkey)
+	case "id":
+		id, err := hex.DecodeString(searchkey)
+		if err == nil {
+			event, err = eh.dbhandler.FindEvent(id)
+		}
+	}
+	if err != nil {
+		fmt.Fprintf(w, "{error %s}", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json;charset=utf8")
+	json.NewEncoder(w).Encode(&event)
+}
+
+func (eh *eventServiceHandler) allEventHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (eh *eventServiceHandler) allEventHandler(w http.ResponseWriter, r *http.Request)  {
-
-}
-
-func (eh *eventServiceHandler) newEventHandler(w http.ResponseWriter, r *http.Request)  {
+func (eh *eventServiceHandler) newEventHandler(w http.ResponseWriter, r *http.Request) {
 
 }
